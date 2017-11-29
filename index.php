bde44bbc34faa10ca8226142d1d68118693bb491
Neto Sales <contato@netosales.com.br>
2017-11-29 20:41:35 -0300

    Arquivos da aula 63, apos erro.

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 require_once("config.php");
-$sql = new Sql();
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-echo json_encode($usuarios);
+//$sql = new Sql();
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//echo json_encode($usuarios);
+$root = new Usuario();
+$root->loadbyId(4);
+echo $root;
+
+
 ?>
